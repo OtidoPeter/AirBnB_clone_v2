50b824ecff86b8f978a2b8a82056c89e4d6a801a
Otido Peter <peterotido@gmail.com>
2023-02-08 00:58:54 -0800

    script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Start a Flask application with routes
+/, /hbnb, /c/<text>
 """
 
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+@app.route('/')
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+    """display text"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>')
+def c_text(text):
+    """display custom text given"""
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    '''Python route'''
+    return 'Python {}'.format(text.replace('_', ' '))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
